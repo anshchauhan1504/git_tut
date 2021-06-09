2b58c93eea57e86a5700cf08b15a13d2e38dd732
anshchauhan1504 <ansh.chauhan15apr@gmail.com>
2021-06-09 18:43:34 +0530

    modified tut2nd

--- tu2.cpp
+++ tu2.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main()
 {
-    cout<<"hello github"<<endl;
+    int n;
+    cout<<"enter n:"<<endl;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        int value;
+        value=n-i+1;
+        while(j<=n-i+1)
+        {
+            cout<<value-j+1;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+
+
 
 }
